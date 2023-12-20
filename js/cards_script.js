16bddd6ffd216bb47e7104085040292cb533eddc
DnNBoss <std.dnnboss@gmail.com>
2023-12-20 21:12:15 +0300

    Remove empty lines

--- js/cards_script.js
+++ js/cards_script.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function(){
     var imagePaths = ["image_1.png", "image_2.png", "image_3.png", "image_4.png", "image_5.png", "image_6.png"];
     var cardsContainer = document.getElementById("cards");
   
-    for (var i = 0; i < imagePaths.length * 2; i++) {
+    for (var i = 0; i < imagePaths.length * 2; i++){
       var card = document.createElement("li");
       card.classList.add("card");
       
@@ -21,5 +21,3 @@ document.addEventListener("DOMContentLoaded", function() {
       cardsContainer.appendChild(card);
     }
   });
-  
-  
