e481efe5bd97ed28ececc91f3b55f5fa5f1841af
DnNBoss <std.dnnboss@gmail.com>
2023-12-30 19:40:00 +0300

    Code refactoring

--- js/cards_script.js
+++ js/cards_script.js
@@ -1,23 +1,41 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var imagePaths = ["image_1.png", "image_2.png", "image_3.png", "image_4.png", "image_5.png", "image_6.png"];
-    var cardsContainer = document.getElementById("cards");
-  
-    for (var i = 0; i < imagePaths.length * 2; i++){
-      var card = document.createElement("li");
-      card.classList.add("card");
-      
-      var backView = document.createElement("div");
-      backView.classList.add("view", "back-view");
-      backView.innerHTML = '<img src="../images/background_image.png" alt="icon">';
-      
-      var frontView = document.createElement("div");
-      frontView.classList.add("view", "front-view");
-      var imageIndex = i % imagePaths.length;
-      frontView.innerHTML = '<img src="../images/' + imagePaths[imageIndex] + '" alt="card-image">';
-      
-      card.appendChild(backView);
-      card.appendChild(frontView);
-      
-      cardsContainer.appendChild(card);
+export function createCards() {
+    let imagePath = ["image_1.png", "image_2.png", "image_3.png", "image_4.png", "image_5.png", "image_6.png"];
+    let amountOfPairs = 2;
+    let amountOfCards = imagePath.length * amountOfPairs;
+    let createdCards = [];
+
+    for (let i = 0; i < amountOfCards; i++) {
+        let imageIndex = i % imagePath.length;
+        let imageName = imagePath[imageIndex];
+        let card = createNewCard(imageName);
+        createdCards.push(card);
     }
-  });
+    addCardsToContainer(createdCards, "cards");
+}
+
+function createNewCard(imageName) {
+    let card = document.createElement("li");
+    card.classList.add("card");
+
+    let backView = createCardView("back-view", '<img src="../images/background_image.png" alt="icon">');
+    let frontView = createCardView("front-view", '<img src="../images/' + imageName + '" alt="card-image">');
+
+    card.appendChild(backView);
+    card.appendChild(frontView);
+
+    return card;
+}
+
+function createCardView(viewClass, content) {
+    let view = document.createElement("div");
+    view.classList.add("view", viewClass);
+    view.innerHTML = content;
+    return view;
+}
+
+function addCardsToContainer(cards, containerId) {
+    let cardsContainer = document.getElementById(containerId);
+    cards.forEach(function (card) {
+        cardsContainer.appendChild(card);
+    });
+}
